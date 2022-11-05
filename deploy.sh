fa70f2f297584112847d056138bae4213453dc0d
Sergey Smirnov <cepera.cMupHoB@gmail.com>
2022-11-05 07:33:52 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,15 @@ cd dist
 # если вы публикуете на пользовательский домен
 # echo 'www.example.com' > CNAME
 
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+
+git push -f git@github.com:morysol/vue-calculator.github.io.git main:gh-pages
+
+# git push -f git@github.com:morysol/vue-calculator.git main:gh-pages
+
+
+
 git init
 git add -A
 git commit -m 'deploy'
